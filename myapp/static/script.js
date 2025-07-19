218f3f9d1404d89ad924fc4c8cd92feaa0761f3d
Friz <70208650+helifriz@users.noreply.github.com>
2025-07-19 08:24:27 -0400

    Update script.js

--- myapp/static/script.js
+++ myapp/static/script.js
@@ -921,7 +921,6 @@ function printFlightLog() {
   const twilightRow = twilight
     ? `<tr><td colspan="2">Civil AM:</td><td colspan="3">${twilight.dawn}</td>` +
       `<td colspan="2">Civil PM:</td><td colspan="3">${twilight.dusk}</td>` +
-      `<td></td>
        </tr>`
     : "";
   const infoTable = `
